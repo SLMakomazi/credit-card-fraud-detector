741b437df7f8eef016aa016ef371f985cee1c393
SLMakomazi <lutho.makomazi@gmail.com>
2025-07-19 09:33:25 +0200

    Update togglz version to 2.9.7 and fix pom.xml configuration

---
+++ src/main/java/com/example/creditcardfrauddetector/CreditCardFraudDetectorApplication.java
@@ -0,0 +1,11 @@
+package com.example.creditcardfrauddetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardFraudDetectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardFraudDetectorApplication.class, args);
+    }
+}
